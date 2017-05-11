2a4716a7a833789a5b557e5ebeceb9c25e2a0e73
azranel <chunkyo@gmail.com>
2017-05-11 18:34:22 +0200

    Change database settings

--- apps/dashboard/config/dev.exs
+++ apps/dashboard/config/dev.exs
@@ -3,8 +3,6 @@ use Mix.Config
 # Configure your database
 config :dashboard, Dashboard.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "dashboard_dev",
   hostname: "localhost",
   pool_size: 10
